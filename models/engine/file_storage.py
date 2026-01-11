78818d371f4c0d331482827f8b255da865691a5a
FabriceMbarushimana <f.mbarushim@alustudent.com>
2026-01-11 09:27:51 -0800

    Add permisions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,11 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -34,7 +39,12 @@ class FileStorage:
         """Deserializes the JSON file to __objects"""
         classes = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
         try:
             with open(self.__file_path, 'r') as file:
